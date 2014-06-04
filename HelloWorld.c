6f5e08cf03f379ab4279973c29e443743613bf90
derson <530193236@qq.com>
2014-06-04 10:13:59 +0800

    I have just commit an C program

---
+++ HelloWorld.c
@@ -0,0 +1,14 @@
+/*
+* I am an greenhand for github. and I also is a primary programmer.
+* This is my first attempt to use github.I want to enjoy github.
+* I want to make friend with programmer all of world!
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World! I am comming! \n");
+    return 0;
+}
